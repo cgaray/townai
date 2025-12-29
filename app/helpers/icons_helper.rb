d81b9fdd76d1a0afcf892a07be65af686090ba38
Christopher Garay <cgaray@users.noreply.github.com>
2025-12-29 14:04:00 -0500

    Revert "Initial pass at attendee model (#8)" (#11)
    This reverts commit 11b10503c1bf657762a722a8fd81d54a86d868db.

--- app/helpers/icons_helper.rb
+++ app/helpers/icons_helper.rb
@@ -102,18 +102,6 @@ module IconsHelper
       <<~SVG
         <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
       SVG
-    when "exclamation-triangle"
-      <<~SVG
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
-      SVG
-    when "currency-dollar"
-      <<~SVG
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-      SVG
-    when "identification"
-      <<~SVG
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
-      SVG
     else
       # Default document icon
       <<~SVG
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "documents/index"
+  get "documents/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -15,15 +17,10 @@ Rails.application.routes.draw do
       post :retry
     end
   end
-
-  resources :people, only: [ :index, :show ]
-
   root "documents#index"
 
   # Admin routes
   namespace :admin do
     resources :api_costs, only: [ :index ]
-    post "people/merge", to: "people#merge", as: :people_merge
-    post "people/unmerge", to: "people#unmerge", as: :people_unmerge
   end
 end
